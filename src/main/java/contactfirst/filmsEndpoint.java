406e7585c6e9b9f331aa76d36ca43eacf0d4e172
Neykhot <maxime.bediguian@laposte.net>
2015-04-01 11:57:35 +0200

    commit fin de seance tp 1/04

--- src/main/java/contactfirst/filmsEndpoint.java
+++ src/main/java/contactfirst/filmsEndpoint.java
@@ -1,56 +1,55 @@
 package contactfirst;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.Namespace;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import org.springframework.ws.server.endpoint.annotation.XPathParam;
 import org.w3c.dom.Element;
+import univ.ups.iaws.Beans.Film;
+import univ.ups.iaws.services.FilmService;
 
-import java.util.List;
+package univ.ups.iaws.Beans.Films;
 
 /**
  * @author franck Silvestre
  */
 @Endpoint
 public class FilmsEndpoint {
-    private Films films;
+    private Film films;
 
     private static final String NAMESPACE_URI = "http://www.omdbapi.com/?s=the+matrix&y=&plot=short&r=json";
 
     
-    public FilmsEndpoint(Films films) {
+    public FilmsEndpoint(Film films) {
         this.films = films;
     }
 
     @PayloadRoot(localPart = "FilmsRequest", namespace = NAMESPACE_URI)
     @ResponsePayload
     @Namespace(prefix = "s", uri=NAMESPACE_URI)
-    public Element handleFilmsRequest(@XPathParam("/s:FilmsRequest/s:title")String title,
+    public Element handleFilmsRequest(@XPathParam("/s:FilmsRequest/s:titre")String titre,
     		@XPathParam("/s:FilmsRequest/s:imdbID")String imdbID,
-    		@XPathParam("s:FilmsRequest/s:year")String year,
-            @XPathParam("s:FilmsRequest/s:type")String type) throws Exception {
+    		@XPathParam("s:FilmsRequest/s:annee") int annee) throws Exception {
 
         // parse le XML de FilmsRequest pour extraire les informations de
-        // title, du imdbID et du semestre  et creer les objets ad-hoc
-        Title title = new Title(title);
-        ID imdbID = new ID(imdbID);
-        Year year = new Year(year);
-        Type type = new Type(type);
+        // title, du imdbID et creer les objets ad-hoc
+        titre = new String(titre);
+        imdbID = new String(imdbID);
 
 
         // invoque le service "releveNoteService" pour récupérer les objets recherchés
         //
-        List<Evaluation> evals = releveNotesService.findAllEvaluationsForAnneeScolaireNiveauAndSemestre(anneeScolaire, niveau, semestre);
+        Film evals = FilmService.findAllFilms(imdbID, titre, annee);
 
         // Transforme en élément XML ad-hoc pour le retour
         // Ici, on prend le parti de renvoyer un fichier XML statique. Il faudrait traiter la
         // liste des évaluations avec une API XML pour fournir l'élément réponse de manière
         // dynamique
 
-        Element elt = XmlHelper.getRootElementFromFileInClasspath("ReleveNotes.xml") ;
+        Element elt = XmlHelper.getRootElementFromFileInClasspath("Film.xml") ;
         return  elt;
 
     }
