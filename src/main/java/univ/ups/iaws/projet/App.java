a6fe4b57172161b27851ce47cc676090a2a78044
ClementCa <clementcarretier@gmail.com>
2015-04-06 13:21:42 +0200

    Hiberntate INSERT INTO selectionner toutes les salles possedant un film selectionner tous les film d'une salle supprimer une salle et un film

--- src/main/java/univ/ups/iaws/projet/App.java
+++ src/main/java/univ/ups/iaws/projet/App.java
@@ -3,7 +3,12 @@ package univ.ups.iaws.projet;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import univ.ups.iaws.Beans.Film;
+import univ.ups.iaws.Beans.Salle;
 import univ.ups.iaws.Hibernate.HibernateUtils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hello world!
@@ -16,18 +21,73 @@ public class App
 
         System.out.println( "Hello World!" );
 
-        HibernateUtils hibernate = new HibernateUtils();
+      /*  HibernateUtils hibernate = new HibernateUtils();
         SessionFactory sessionFactory = hibernate.getSessionFactory();
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        session.beginTransaction();*/
+        List<Film> films = new ArrayList<Film>();
+
         Film film = new Film();
         film.setAnnee(10);
-        film.setImdbId("aze");
-        film.setTitre("titre1");
-        session.save(film);
-        session.flush();
+        film.setImdbId("azertyu");
+        film.setTitre("film1");
+        films.add(film);
+
+        film = new Film();
+        film.setAnnee(10);
+        film.setImdbId("qsdfgh");
+        film.setTitre("film2");
+        films.add(film);
+
+        film = new Film();
+        film.setAnnee(10);
+        film.setImdbId("wxcvbn");
+        film.setTitre("film3");
+        films.add(film);
+
+        film = new Film();
+        film.setAnnee(10);
+        film.setImdbId("pmlk");
+        film.setTitre("film4");
+        films.add(film);
+
+
+        film = new Film();
+        film.setAnnee(10);
+        film.setImdbId("654fg");
+        film.setTitre("film5");
+        films.add(film);
+
+
+        Salle salle = new Salle();
+        salle.setNbSalle(5);
+        salle.setVille("Toulouse");
+
+        GestionnaireLiaison gestionnaire = new GestionnaireLiaison();
+        gestionnaire.saveAll(salle,films);
+
+
+        Salle salle2 = new Salle();
+        salle2.setNbSalle(2);
+        salle2.setVille("Paris");
+        gestionnaire.saveAll(salle2,films);
+
+        List<Salle> list = gestionnaire.getSallesByFilm(film);
+        for(Salle sallels: list){
+            System.out.println(" id "+sallels.getId()+" ville "+sallels.getVille());
+        }
+
+        List<Film> listFilm = gestionnaire.getFilmsBySalle(salle);
+        for(Film filmS: listFilm){
+            System.out.println(" id "+filmS.getId()+" ville "+filmS.getTitre());
+        }
+        gestionnaire.deleteElement(salle,film);
+
+
+        /*session.flush();
         session.getTransaction().commit();
         session.close();
         hibernate.shutdown();
+    */
     }
 }
---
+++ src/main/java/univ/ups/iaws/projet/GestionnaireSalle.java
@@ -0,0 +1,46 @@
+package univ.ups.iaws.projet;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import univ.ups.iaws.Beans.Film;
+import univ.ups.iaws.Beans.Salle;
+import univ.ups.iaws.Hibernate.HibernateUtils;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by clement on 06/04/2015.
+ */
+public class GestionnaireSalle {
+    private Salle salle;
+    private Session session;
+    private HibernateUtils hibernate;
+    public GestionnaireSalle(){
+        HibernateUtils hibernate = new HibernateUtils();
+        SessionFactory sessionFactory = hibernate.getSessionFactory();
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+    }
+    public GestionnaireSalle(Session session){
+        this.session = session;
+    }
+    public Salle saveSalle(Salle newSalle){
+        session.saveOrUpdate(newSalle);
+        return newSalle;
+    }
+
+    public void deleteSalle(Salle salle){
+        session.delete(salle);
+    }
+
+    public void shutDownFactory(){
+        session.close();
+        hibernate.shutdown();
+    }
+
+
+
+
+}
