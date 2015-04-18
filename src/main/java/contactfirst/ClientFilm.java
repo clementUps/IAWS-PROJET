62d4a0924a4914a683efc1bfbbdcf3239533548a
ClementCa <clementcarretier@gmail.com>
2015-04-18 20:42:25 +0200

    Modification En RESTFull

---
+++ src/main/java/contactfirst/ClientFilm.java
@@ -0,0 +1,49 @@
+package contactfirst;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+
+
+/**
+ * Created by clement on 18/04/2015.
+ */
+public class ClientFilm {
+    private static final String API_URI = "http://www.omdbapi.com/?";
+    private static final String TITRE_URI = "s=";
+    private static final String LIST_TITRE_URI = "t=";
+    private static final String ANNEE_URI = "&y=";
+    private static final String END_URI = "&plot=short&r=xml";
+
+
+    public static InputStream getEvals(String titre,String annee,boolean isTitre){
+        final Client c = ClientBuilder.newClient();
+    	String build =  API_URI+(isTitre?TITRE_URI:LIST_TITRE_URI)+titre+ANNEE_URI+annee+END_URI;
+        final WebTarget wt = c.target(build);
+        try {
+        	System.out.println(wt.getUri());
+            Response response = wt.request(MediaType.APPLICATION_XML).get();
+            InputStream in = response.readEntity(InputStream.class);
+            System.out.println("string response: " + response.readEntity(String.class));
+            return in;
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static void main( String[] args ){
+        ClientFilm c = new ClientFilm();
+        try {
+            (c.getEvals("matrix", "",true)).close();
+            (c.getEvals("matrix", "",false)).close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/contactfirst/Endpoint.java
+++
@@ -1,7 +0,0 @@
-package contactfirst;
-
-/**
- * Created by Max on 01/04/2015.
- */
-public @interface Endpoint {
-}
